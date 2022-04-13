01b511147bbd90ce30a3b94ee361f11afc8372b5
lexxam <lexamishkin72@gmail.com>
2022-04-13 20:17:23 +0400

    Implemented test coverage of the main functions of the class

--- test/test.cpp
+++ test/test.cpp
@@ -27,3 +27,47 @@ TEST(node, copyOperator)
 	ASSERT_EQ(copyNode.left(), nullptr);
 	ASSERT_EQ(copyNode.right(), nullptr);
 }
+
+TEST(set, intConstructor)
+{
+	set<int> testSet;
+	ASSERT_EQ(testSet.find(3), 0);
+}
+
+TEST(set, findTest)
+{
+	set<int> testSet;
+	testSet.insert(200);
+	testSet.insert(4);
+
+	EXPECT_TRUE(testSet.find(200));
+	EXPECT_TRUE(testSet.find(4));
+	EXPECT_TRUE(!testSet.find(1));
+}
+
+TEST(set, insertTest)
+{
+	set<int> testSet;
+	testSet.insert(3);
+	testSet.insert(1);
+	testSet.insert(6);
+
+	ASSERT_EQ(testSet.find(3), 1);
+	ASSERT_EQ(testSet.find(2), 0);
+}
+
+TEST(set, deleteTest)
+{
+	set<int> testSet;
+	testSet.insert(3);
+	testSet.insert(-1);
+	testSet.insert(-3);
+	testSet.insert(8);
+	testSet.insert(2);
+	testSet.insert(7);
+
+	testSet.erase(8);
+	testSet.erase(-1);
+
+	EXPECT_TRUE(testSet.find(2) && testSet.find(3) && testSet.find(-3) && testSet.find(7) && !testSet.find(-1) && !testSet.find(8));
+}
